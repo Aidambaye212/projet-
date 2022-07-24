af1ee75ef8e23313b4d298e4cbba5ca42773068e
Aidambaye212 <108492407+Aidambaye212@users.noreply.github.com>
2022-07-24 08:54:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const items = document.querySelectorAll('img');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+var like = document.querySelectorAll(".far");
+var count = 0;
+
+function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count < nbSlide - 1){
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].classList.add('active')
+    console.log(count);
+    
+}
+suivant.addEventListener('click', slideSuivante)
+
+
+function slidePrecedente(){
+    items[count].classList.remove('active');
+
+    if(count > 0){
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    items[count].classList.add('active')
+    
+}
+precedent.addEventListener('click', slidePrecedente)
+
+function keyPress(e){
+    console.log(e);
+    
+    if(e.keyCode === 37){
+        slidePrecedente();
+    } else if(e.keyCode === 39){
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress)
+
+//------------------BOUTON LIKE----------------------------
+like[i].addEventListener('click' , function(e){
+    if(e.target.classList.contains('far'))
+    {
+        e.target.classList.replace('far', 'fas');
+    }
+    else
+    {
+        e.target.classList.replace('fas', 'far');
+    }
+})
